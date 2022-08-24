f41b5dc81b5df54772dd2fe56a05865259cadeaf
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-08-25 03:24:06 +0600

    JSX Dynamic Content & Dynamic style in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const number = 5555;
+const singer = {
+	name:'Dr. Mahfuz',
+	job:'Singer'
+}
+const singer2 = {
+	name:'Eva Rahman',
+	job:'singer2'
 }
 
+/*Start Style*/
+const singerStyle = {
+	color:'Purple',
+	backgroundColor:'white'
+}
+/*End Style*/
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>JSX</h1>
+
+			{/*Custom HTML*/}
+			<div id="countries" className="countries">
+				<p></p>
+				<p></p>
+				<p></p>
+			</div>
+
+			{/*Custom HTML Adding*/}
+			<div className="container">
+				<h3>Hello Dude React.How are you?</h3>
+			</div>
+
+			<div className="music">
+				<p>Name: {number}</p>
+				<p>Name: {2222+number}</p>
+				<p style={singerStyle}>Name: {singer.name} {singer.job}</p>
+				<p style={{color:'blue',backgroundColor:'yellow'}}>Name: {singer2.name} {singer2.job}</p>
+			</div>
+
+				<p>Edit <code>src/App.js</code> and save to reload.</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+			</header>
+		</div>
+	);
+}
 export default App;
